d3b0b824dd989b6d58684684530e5566a4962981
armaan535kh <kh.armaan535@gmail.com>
2024-08-27 21:18:11 -0700

    Initial Commit

---
+++ Survey Form/Survey_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Survey_FormApp.swift
+//  Survey Form
+//
+//  Created by Armaan Khan  on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Survey_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
